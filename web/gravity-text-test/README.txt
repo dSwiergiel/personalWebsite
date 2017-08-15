08884d7785e8624731e8844330b218af7a0d435f
Deven Swiergiel <dswiergiel@albany.edu>
2017-08-15 18:08:50 -0400

    adds testing files

---
+++ web/gravity-text-test/README.txt
@@ -0,0 +1,7 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/mayurbirle/pen/gWbvLE.
+
+ Inspire by Google Gravity......
+
+Give Gravity to each chracter you write on the BOX
+
+# Gravity Text , throw the word
---
+++ web/gravity-text-test/js/index.js
@@ -0,0 +1,64 @@
+var demo = new Vue({
+    el: '#demo',
+    data: {
+      message: 'Gravity Text',
+      getRandomColor: '#03a9f4'
+    },
+    methods: {
+      getColor: function () {
+      console.log('sadasd')
+         var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+      }
+    }
+})
+
+
+$(document).ready(function() {
+
+	var gr_x = document.getElementById("gravity-x").value;
+  	var gr_y = document.getElementById("gravity-y").value;
+
+	$(".start-g").click(function(){
+		$("span").addClass("test");
+		$(".test").throwable({
+						drag:true,
+                        gravity:{x:gr_x,y:gr_y},
+                        impulse:{
+                            f:152,
+                            p:{x:0,y:1.5}
+                        },
+                        shape:"circle",
+                        autostart:false,
+                        bounce:20,
+                        damping:13,
+                        
+		});
+
+	});
+
+	$(".stop-g").click(function(){
+		$(".test").throwable({
+			gravity:{x:0,y:0},
+		});
+	});
+
+	$("#gravity-x, #gravity-y").click(function(){
+		var gr_x = document.getElementById("gravity-x").value;
+  	var gr_y = document.getElementById("gravity-y").value;
+		$(".test").throwable({
+			gravity:{x:gr_x,y:gr_y},
+		});
+
+	});
+});
+
+$(document).ready(function(){
+$(".btn").click(function(){$(".main-c").slideToggle("slow");});
+$(".close").click(function(){$(".main-c").slideUp();});
+
+});
