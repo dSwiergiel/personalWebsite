7d5493df7ef5b2cd615fbf115147c33d38f6d802
Deven Swiergiel <dswiergiel@albany.edu>
2017-11-04 15:31:19 -0400

    fixes hamburger button bug

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-toggle_nav_container();
-gotoByScroll();
+	toggle_nav_container();
+	gotoByScroll();
 
 
 });
@@ -13,23 +13,22 @@ var toggle_nav_container = function () {
 
 
 
-	var 	$toggleButton = $('#toggle_m_nav');
-			$navContainer = $('#m_nav_container');
-			$menuButton = $('#m_nav_menu')
-			$menuButtonBars = $('.m_nav_ham');
-			$wrapper = $('#wrapper');
+	var $toggleButton = $('#toggle_m_nav');
+	$navContainer = $('#m_nav_container');
+	$menuButton = $('#m_nav_menu')
+	$menuButtonBars = $('.m_nav_ham');
+	$wrapper = $('#wrapper');
 
 	// toggle the container on click of button (can be remapped to $menuButton)
 
-	$toggleButton.on("click", function(){
+	$toggleButton.on("click", function () {
 
 		// declare a local variable for the window width
 		var $viewportWidth = $(window).width();
 
 		// if statement to determine whether the nav container is already toggled or not
 
-		if($navContainer.is(':hidden'))
-		{	
+		if ($navContainer.is(':hidden')) {
 			$wrapper.removeClass('closed_wrapper');
 			$wrapper.addClass("open_wrapper");
 			$navContainer.slideDown(200).addClass('container_open').css("z-index", "2");
@@ -40,8 +39,7 @@ var toggle_nav_container = function () {
 			$("#m_ham_2").addClass("m_nav_ham_2_open");
 			$("#m_ham_3").addClass("m_nav_ham_3_open");
 		}
-		else
-		{
+		else {
 			$navContainer.css("z-index", "0").removeClass('container_open').slideUp(200)
 			$menuButtonBars.removeClass('button_open')
 			$menuButtonBars.addClass('button_closed')
@@ -59,20 +57,36 @@ var toggle_nav_container = function () {
 //the div on the page whose ID matches said value. This only works if you use 
 //a consistent naming scheme for the navbar anchors and div IDs
 
-var gotoByScroll = function (){
+var gotoByScroll = function () {
 
-	$(".m_nav_item a").on("click", function(e) {
+	var $toggleButton = $('#toggle_m_nav');
+	$navContainer = $('#m_nav_container');
+	$menuButton = $('#m_nav_menu')
+	$menuButtonBars = $('.m_nav_ham');
+	$wrapper = $('#wrapper');
+
+	$(".m_nav_item a").on("click", function (e) {
 
 		e.preventDefault();
 		// var $divID =$(this).attr("href");
 		// var $scrollToDiv = "$(" + "'" + $divID + "'" +")";
-		
+
 		$('html,body').animate({
-   scrollTop: $($(this).attr("href")).offset().top - 50
-}, "slow");
+			scrollTop: $($(this).attr("href")).offset().top - 50
+		}, "slow");
+
+		// after a selection close the hamburger menu
+		$navContainer.css("z-index", "0").removeClass('container_open').slideUp(200)
+		$menuButtonBars.removeClass('button_open')
+		$menuButtonBars.addClass('button_closed')
+		$wrapper.removeClass('open_wrapper')
+		$wrapper.addClass("closed_wrapper")
+		$("#m_ham_1").removeClass("m_nav_ham_1_open");
+		$("#m_ham_2").removeClass("m_nav_ham_2_open");
+		$("#m_ham_3").removeClass("m_nav_ham_3_open");
 
 	});
-		
+
 }
 
 
